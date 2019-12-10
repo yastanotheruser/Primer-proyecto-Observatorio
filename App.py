c603c32b6dedbd85cbb404e44cf42cfaef5590fa
My Name <me@example.com>
2019-12-09 22:17:16 -0500

    configuración de base de datos

--- App.py
+++ App.py
@@ -2,17 +2,21 @@
    .. moduleauthor:: Alejandro Burgos
 
 """
+import os
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+load_dotenv()
+
 app = Flask(__name__)
 """Creates the Flask instance.
 
 :param __name__: Is the name of the current Python module.
 """
-app.config["MYSQL_HOST"] = 'localhost'
-app.config["MYSQL_USER"] = 'root'
-app.config["MYSQL_PASSWORD"] = 'password'
-app.config["MYSQL_DB"] = 'flaskcontacts'
+app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
 mysql = MySQL(app)
 
 @app.route('/')
